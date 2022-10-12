4864971956091f089b9016a0b0901cccfc552e8f
jasonyuan <jasonyuan@tuofaninfo.com>
2022-10-13 00:21:00 +0800

    0.0.7

--- test/module.test.js
+++ test/module.test.js
@@ -2,40 +2,63 @@ const path = require("path");
 const MicroModule = require("../lib/module");
 
 describe("module", function () {
-  test("Calling with the wrong format should throw an error", async () => {
+  test("load ok", () => {
     const rootDir = path.join(__dirname, "module");
-    const servicesDir = "./standard";
+    const servicesDir = "./load";
     const microModule = new MicroModule();
-    microModule.load(rootDir, servicesDir);
+    const cache = microModule.load(rootDir, servicesDir);
 
-    const callList = [
-      ["_auth", "user", "list"],
-      ["auth", "_user", "list"],
-      ["auth", "user", "_list"],
-    ];
-
-    for (const callPath of callList) {
-      await expect(microModule.call(...callPath)).rejects.toThrow();
-    }
+    expect(typeof cache.pp).toBe("object");
+    expect(typeof cache.pp.index).toBe("object");
+    expect(typeof cache.pp.index.test).toBe("object");
   });
 
-  test("Calling the correct path executes successfully", async () => {
+  test("parseInfo _", () => {
+    const microModule = new MicroModule();
+    const info = microModule.parseInfo("_hello");
+    expect(info.modifier).toBe("_");
+    expect(info.name).toBe("hello");
+  });
+
+  test("parseInfo $", () => {
+    const microModule = new MicroModule();
+    const info = microModule.parseInfo("$hello");
+    expect(info.modifier).toBe("$");
+    expect(info.name).toBe("hello");
+  });
+
+  test("parseInfo error", () => {
+    const microModule = new MicroModule();
+    const info = microModule.parseInfo("+heeeello");
+    expect(info).toBe(null);
+  });
+
+  test("getFunc ok", async () => {
     const rootDir = path.join(__dirname, "module");
     const servicesDir = "./standard";
     const microModule = new MicroModule();
     microModule.load(rootDir, servicesDir);
 
-    await expect(
-      microModule.call("auth", "user", "list", {
-        data: { from: "test" },
-      })
-    ).resolves.toStrictEqual({
-      from: "test",
-      data: [
-        {
-          name: "jason",
-        },
-      ],
+    const funcInfo = await microModule.getFunc("auth", "user", "list", {
+      data: { from: "test" },
     });
+
+    expect(funcInfo.name).toBe("list");
+    expect(typeof funcInfo.fn).toBe("function");
+    expect(funcInfo.service.name).toBe("auth");
+    expect(funcInfo.logic.name).toBe("user");
+  });
+
+  test("getFunc error", async () => {
+    const rootDir = path.join(__dirname, "module");
+    const servicesDir = "./standard";
+    const microModule = new MicroModule();
+    microModule.load(rootDir, servicesDir);
+
+    const funcInfo = await microModule.getFunc("auth1", "user", "list", {
+      data: { from: "test" },
+    });
+
+    expect(funcInfo).toBeNull();
   });
 });
---
+++ test/module/load/$pp/index.js
@@ -0,0 +1,8 @@
+const test = async () => {};
+
+const $hello = async () => {};
+
+module.exports = {
+  test,
+  $hello,
+};
