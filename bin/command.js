281a7e86ca2b73f147b550c611ef062c081a3883
jasonyuan <jasonyuan@tuofaninfo.com>
2022-10-13 14:57:06 +0800

    bugfix

--- bin/command.js
+++ bin/command.js
@@ -7,17 +7,6 @@ const package = require("../package.json");
 
 program.version(package.version);
 
-const serverJS = `
-  const microServer = require('micro-server');
-  microServer.start();
-`;
-
-program.command("init").action(async () => {
-  // const projectDir = process.cwd();
-  // const serverFile = path.join(projectDir, "./server.js");
-  // const serverStat = await fsp.stat(serverFile);
-  // console.log(serverStat);
-  // // fse.readFile(process.cwd(), "./config");
-});
+program.command("init").action(async () => {});
 
 program.parse();
